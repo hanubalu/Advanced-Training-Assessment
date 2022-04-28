c6ca7cb0b655d77031ef0abddbdfab2067199612
hanubalu <hanubalu1234@gmail.com>
2022-04-28 23:43:46 +0530

    Advanced Training Assessment

---
+++ Day-1-projects/src/com/statement7/stream_based_Program/AgeNotWithInRangeException.java
@@ -0,0 +1,9 @@
+package com.statement7.stream_based_Program;
+
+public class AgeNotWithInRangeException {
+	
+	     public String toString()
+	     {
+	          return ("Age is not between 15 and 21. please ReEnter the Age");
+	     }
+	}
---
+++ Day-1-projects/src/com/statement7/stream_based_Program/NameNotValidException.java
@@ -0,0 +1,9 @@
+package com.statement7.stream_based_Program;
+
+public class NameNotValidException extends Exception{
+
+	public String validname()
+    {
+         return ("Name is not Valid..Please ReEnter the Name");
+    }
+}
