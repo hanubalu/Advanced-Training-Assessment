454ea8b448deff7ac84553cbb23a0c81172af14d
hanubalu <hanubalu1234@gmail.com>
2022-04-29 16:26:40 +0530

    Problem Statement 16

---
+++ Day-2-Projects/src/com/statement16/Climbing_Stairs/Climbing_Stairs.java
@@ -0,0 +1,32 @@
+package com.statement16.Climbing_Stairs;
+
+import java.util.Scanner;
+
+public class Climbing_Stairs {
+
+	 
+    static int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+ 
+  
+    static int countWays(int s)
+    {
+        return fib(s + 1);
+    }
+ 
+       public static void main(String args[]) {
+    
+    	Scanner scanner=new Scanner(System.in);
+    System.out.println("Enter the Number");
+    int n=scanner.nextInt();
+    
+    {
+        
+        System.out.println("Number of ways = " + countWays(n));
+    }
+ }
+}
