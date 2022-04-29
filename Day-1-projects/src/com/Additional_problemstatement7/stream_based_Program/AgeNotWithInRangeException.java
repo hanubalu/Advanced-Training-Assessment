0eb5ff6ad034fae72543c27f965da956799c173a
hanubalu <hanubalu1234@gmail.com>
2022-04-29 13:46:07 +0530

    Extra Program For Problem Statement 7.2

---
+++ Day-1-projects/src/com/Additional_problemstatement7/stream_based_Program/AgeNotWithInRangeException.java
@@ -0,0 +1,11 @@
+package com.Additional_problemstatement7.stream_based_Program;
+
+
+@SuppressWarnings("serial")
+class AgeNotWithInRangeException extends Exception
+{
+     public String toString()
+     {
+          return ("Age is not between 15 and 21. please ReEnter the Age");
+     }
+}
---
+++ Day-1-projects/src/com/Additional_problemstatement7/stream_based_Program/Demo.java
@@ -0,0 +1,11 @@
+package com.Additional_problemstatement7.stream_based_Program;
+
+@SuppressWarnings("serial")
+
+public class Demo extends Exception{
+
+	public String validname()
+    {
+         return ("Name is not Valid..Please ReEnter the Name");
+    }
+}
