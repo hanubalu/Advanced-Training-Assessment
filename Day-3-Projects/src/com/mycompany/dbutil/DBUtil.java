fe22fb78d544c460ad44c0c26ada7cbdc3ce4b4e
hanubalu <hanubalu1234@gmail.com>
2022-05-02 17:08:11 +0530

    Day-3 Assessments

---
+++ Day-3-Projects/src/com/mycompany/dbutil/DBUtil.java
@@ -0,0 +1,36 @@
+package com.mycompany.dbutil;
+
+import java.sql.*;
+
+public class DBUtil {
+
+    //static method that creates a connection to the database and return the connection object
+    public static Connection getConnection()
+    {
+        Connection conn = null;
+        try{
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        	//DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/productdb", "root", "balu@1234");
+           
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    //close connection
+    public static void closeConnection(Connection conn)
+    {
+        try{
+            conn.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
